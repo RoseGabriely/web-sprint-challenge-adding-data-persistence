8eeee5a2f54ac72d4c7be310b2890ff13bdea908
RoseGabriely <rosegabriely14@gmail.com>
2021-11-05 19:48:57 -0400

    finished mvp

--- api/task/model.js
+++ api/task/model.js
@@ -1,11 +1,42 @@
-const express = require("express");
-const router = express.Router();
+const db = require("../../data/dbConfig");
 
-router.get("/", (req, res, next) => {
-  res.json("connected?");
-});
-router.post("/", (req, res, next) => {
-  res.json("connected?2");
-});
+const get = async () => {
+  const tasks = await db("tasks as t")
+    .leftJoin("projects as p", "t.task_id", "p.project_id")
+    .select("t.*", "p.project_name", "p.project_description")
+    .orderBy("t.task_id", "asc");
+  return tasks.map((task) => {
+    return {
+      task_id: task.task_id,
+      task_notes: task.task_notes,
+      task_completed: task.task_completed ? true : false,
+      task_description: task.task_description,
+      project_id: task.project_id,
+      project_description: task.project_description,
+      project_name: task.project_name,
+    };
+  });
+};
+const create = (task) => {
+  return db("tasks")
+    .insert(task)
+    .then(([id]) => {
+      return db("tasks")
+        .where("task_id", id)
+        .first()
+        .then((task) => {
+          return {
+            task_id: task.task_id,
+            task_notes: task.task_notes,
+            task_completed: task.task_completed ? true : false,
+            task_description: task.task_description,
+            project_id: task.project_id,
+          };
+        });
+    });
+};
 
-module.exports = router;
+module.exports = {
+  get,
+  create,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const Tasks = require("./model");
 
 router.get("/", (req, res, next) => {
-  res.json("connected?");
+  Tasks.get()
+    .then((task) => {
+      res.json(task);
+    })
+    .catch(next);
 });
 router.post("/", (req, res, next) => {
-  res.json("connected?2");
+  Tasks.create(req.body)
+    .then((newTask) => {
+      res.json(newTask);
+    })
+    .catch(next);
 });
 
 module.exports = router;
