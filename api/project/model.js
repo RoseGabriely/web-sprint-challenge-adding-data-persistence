a60c0ea2e4a58eb6697a583cd19e1404d9d1e6d7
RoseGabriely <rosegabriely14@gmail.com>
2021-11-05 18:30:45 -0400

    completed projects endpoints and access functions

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,37 @@
-// build your `Project` model here
+const db = require("../../data/dbConfig");
+
+async function get() {
+  const projects = await db("projects");
+
+  return projects.map((project) => {
+    return {
+      project_id: project.project_id,
+      project_name: project.project_name,
+      project_completed: project.project_completed ? true : false,
+      project_description: project.project_description,
+    };
+  });
+}
+
+function insert(project) {
+  return db("projects")
+    .insert(project)
+    .then(([id]) => {
+      return db("projects")
+        .where("project_id", id)
+        .first()
+        .then((project) => {
+          return {
+            project_id: project.project_id,
+            project_name: project.project_name,
+            project_completed: project.project_completed ? true : false,
+            project_description: project.project_description,
+          };
+        });
+    });
+}
+
+module.exports = {
+  get,
+  insert,
+};
--- api/project/router.js
+++ api/project/router.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const Projects = require("./model.js");
 
 router.get("/", (req, res, next) => {
-  res.json("connected?");
+  Projects.get()
+    .then((projects) => {
+      res.json(projects);
+    })
+    .catch(next);
 });
+
 router.post("/", (req, res, next) => {
-  res.json("connected?2");
+  Projects.insert(req.body)
+    .then((newProj) => {
+      res.json(newProj);
+    })
+    .catch(next);
 });
 
 module.exports = router;
