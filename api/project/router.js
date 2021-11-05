66d55d57cea1699c0403c1cc1546ec5c0ebe9a4b
RoseGabriely <rosegabriely14@gmail.com>
2021-11-05 17:39:46 -0400

    configured server and set up routing

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,11 @@
-// build your `/api/projects` router here
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.json("connected?");
+});
+router.post("/", (req, res, next) => {
+  res.json("connected?2");
+});
+
+module.exports = router;
